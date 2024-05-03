e5e9609ab41484c3bbf8a05b289e8274331429df
Kim JaeMin <2098kjm@naver.com>
2024-05-03 17:24:00 +0900

    first commit

---
+++ src/main/java/com/example/iSale/ISaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.iSale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iSale/ISaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iSale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
