f0ca9c9d528ff6d2f52116793f9ba56012c39c9b
Kim JaeMin <2098kjm@naver.com>
2024-05-07 18:08:06 +0900

    iSale CR

---
+++ src/main/java/com/example/iSale/service/ISaleService.java
@@ -0,0 +1,11 @@
+package com.example.iSale.service;
+
+import com.example.iSale.dto.request.ISaleRequest;
+import com.example.iSale.global.domain.entity.ISale;
+import java.util.List;
+
+public interface ISaleService {
+    void save(ISaleRequest req);
+    ISale getISaleById(String id);
+    List<ISale> getAllISale();
+}
